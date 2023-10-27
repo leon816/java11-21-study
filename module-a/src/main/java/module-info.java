755945fbdb21f5e532f07dc245b018a0bad77ad1
liliang <*#1nokiasbell>
2023-10-27 15:40:49 +0800

    update

--- module-a/src/main/java/module-info.java
+++ module-a/src/main/java/module-info.java
@@ -6,6 +6,6 @@ import com.a.test.TestInterface;
  */module module.a {
 	exports com.a.test;
 	opens com.a.test; //开放反射权限
-	requires transitive java.logging;  //传递依赖
+	requires transitive java.logging;  //logging可被传递依赖给requires module.a的模块
 	uses TestInterface; //需要外部模块实现的接口
 }
